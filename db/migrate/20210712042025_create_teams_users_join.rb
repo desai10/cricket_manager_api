2246723dc757f1551c8a46f306029ca6aed91283
Sriram Desai <isriramdesai@gmail.com>
2021-07-12 10:08:02 +0530

    created team and user model; and their db migrations

---
+++ db/migrate/20210712042025_create_teams_users_join.rb
@@ -0,0 +1,16 @@
+class CreateTeamsUsersJoin < ActiveRecord::Migration[6.1]
+  def up
+    create_table :teams_users, :id => false do |t|
+
+      t.integer "team_id"
+      t.integer "user_id"
+
+      t.timestamps
+    end
+    add_index("teams_users", ["team_id", "user_id"])
+
+    def down
+      delete_table :teams_users
+    end
+  end
+end
